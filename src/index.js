ad92a50b8385acbbc23fb943b770bdd54c01737e
guiprj <guilhermecamargo90@hotmail.com>
2021-01-11 16:59:37 -0300

    Atualizando index

--- src/index.js
+++ src/index.js
@@ -1,36 +1,42 @@
 const bot = require("venom-bot");
 const banco = require("./banco");
 const stages = require("./stages");
+const http = require('http')
 
-bot.create().then((client) => start(client));
-function start(client) {
-  client.onMessage((message) => {
-    let resp = stages.step[getStage(message.from)].obj.execute(
-      message.from,
-      message.body,
-      message.sender.name
-    );
-    for (let index = 0; index < resp.length; index++) {
-      const element = resp[index];
-      client.sendText(message.from, element);
-    }
-  });
-}
-
-function getStage(user) {
-  if (banco.db[user]) {
-    //Se existir esse numero no banco de dados
-    return banco.db[user].stage;
-  } else {
-    //Se for a primeira vez que entra e contato
-    banco.db[user] = {
-      stage: 0,
-      itens: [],
-    };
-    return banco.db[user].stage;
+var app = http.createServer(function (request, response) {
+  bot.create().then((client) => start(client));
+  function start(client) {
+    client.onMessage((message) => {
+      let resp = stages.step[getStage(message.from)].obj.execute(
+        message.from,
+        message.body,
+        message.sender.name
+      );
+      for (let index = 0; index < resp.length; index++) {
+        const element = resp[index];
+        client.sendText(message.from, element);
+      }
+    });
   }
-}
 
-process.on('SIGINT', function() {
-  client.close();
-});
+  function getStage(user) {
+    if (banco.db[user]) {
+      //Se existir esse numero no banco de dados
+      return banco.db[user].stage;
+    } else {
+      //Se for a primeira vez que entra e contato
+      banco.db[user] = {
+        stage: 0,
+        itens: [],
+      };
+      return banco.db[user].stage;
+    }
+  }
+
+  process.on('SIGINT', function () {
+    client.close();
+  });
+})
+app.listen(process.env.PORT || 3000)
+
+
