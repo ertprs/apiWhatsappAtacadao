9a5d3f5c6204227b80f546c20e02d8e7258b9053
guiprj <guilhermecamargo90@hotmail.com>
2021-01-15 16:30:03 -0300

    atualizando server e index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const bot = require("venom-bot");
 const banco = require("./banco");
 const stages = require("./stages");
 
-const app = bot.create().then((client) => start(client));
-function start(client) {
+let app = bot.create().then((client) => start(client));
+app = function start(client) {
     client.onMessage((message) => {
       let resp = stages.step[getStage(message.from)].obj.execute(
         message.from,
