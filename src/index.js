a8576289ac9610e8249e017ad0fc93b69d8e9303
guiprj <guilhermecamargo90@hotmail.com>
2021-01-15 15:51:45 -0300

    criando servidor

--- src/index.js
+++ src/index.js
@@ -2,35 +2,37 @@ const bot = require("venom-bot");
 const banco = require("./banco");
 const stages = require("./stages");
 
-  bot.create().then((client) => start(client));
-  function start(client) {
+bot.create().then((client) => start(client));
+const app = function start(client) {
     client.onMessage((message) => {
       let resp = stages.step[getStage(message.from)].obj.execute(
         message.from,
         message.body,
         message.sender.name
-      );
-      for (let index = 0; index < resp.length; index++) {
-        const element = resp[index];
-        client.sendText(message.from, element);
-      }
-    });
-  }
-
-  function getStage(user) {
-    if (banco.db[user]) {
-      //Se existir esse numero no banco de dados
-      return banco.db[user].stage;
-    } else {
-      //Se for a primeira vez que entra em contato
-      banco.db[user] = {
-        stage: 0,
-        itens: [],
-      };
-      return banco.db[user].stage;
+        );
+        for (let index = 0; index < resp.length; index++) {
+          const element = resp[index];
+          client.sendText(message.from, element);
+        }
+      });
     }
-  }
+    
+    function getStage(user) {
+      if (banco.db[user]) {
+        //Se existir esse numero no banco de dados
+        return banco.db[user].stage;
+      } else {
+        //Se for a primeira vez que entra em contato
+        banco.db[user] = {
+          stage: 0,
+          itens: [],
+        };
+        return banco.db[user].stage;
+      }
+    }
+
+    module.exports = app
 
 
-
+  
 
