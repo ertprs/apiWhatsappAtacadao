74a2800c1837aee8cba747bcf092c82050237f99
guiprj <guilhermecamargo90@hotmail.com>
2021-01-15 15:59:11 -0300

    atualizando index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const bot = require("venom-bot");
 const banco = require("./banco");
 const stages = require("./stages");
 
-bot.create().then((client) => start(client));
-const app = function start(client) {
+const app = bot.create().then((client) => start(client));
+function start(client) {
     client.onMessage((message) => {
       let resp = stages.step[getStage(message.from)].obj.execute(
         message.from,
